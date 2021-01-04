20588717377dc5946e616d5d463f60e626b8ad19
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-01-04 14:07:06 -0500

    added aws imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 
 function App() {
+
   return (
     <div>
       <div className="nav-bar">
         <div className="nav-item">Home</div>
         <div className="nav-item">About</div>
         <div className="nav-item">Sign Up / Login</div>
+      </div>
+      <div className="login-signup-form">
+
       </div>
     </div>
   );

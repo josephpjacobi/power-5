53ad414dc06062ded38ca6cf70cf700291b00713
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-01-23 16:46:29 -0500

    displayed custom user attributes on the screen

--- src/App.js
+++ src/App.js
@@ -20,9 +20,19 @@ function App() {
     });
   }, []);
 
+  const logAttrbiutes = (user) => {
+    console.log(`user: ${Object.keys(user.attributes)}`);
+  }
+
   return authState === AuthState.SignedIn && user ? (
+    // custom:last_name,email_verified,custom:first_name,phone_number_verified,phone_number,email
     <div className="App">
       <div>Hello, {user.username}</div>
+      <div>First Name: {user.attributes["custom:first_name"]}</div>
+      <div>Last Name: {user.attributes["custom:last_name"]}</div>
+      <div>Phone Number: {user.attributes.phone_number}</div>
+      <div>Email: {user.attributes.email}</div>
+      <button onClick={() => logAttrbiutes(user)}>User Attributes</button>
       <AmplifySignOut />
     </div>
   ) : (
